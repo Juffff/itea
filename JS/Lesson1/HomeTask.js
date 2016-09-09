57c2462b2d77dde6053160dbdcc49a65198e219a
Voytsekhovskiy Vladimir <vvo@corp.muk.ua>
2016-09-09 14:20:34 +0300

    js hw l1 ready

--- JS/Lesson1/HomeTask.js
+++ JS/Lesson1/HomeTask.js
@@ -21,3 +21,58 @@
     //Напишите код, который для каждого элемента <li> выведет:
     //Текст непосредственно в нём (без подразделов).
     //Количество вложенных в него элементов <li> – всех, с учётом вложенных.
+
+        var liArray = document.getElementsByTagName("li");
+        for(var i = 0; i<liArray.length; i++){
+         console.log(liArray[i].firstChild.data + " child li count = " + liArray[i].getElementsByTagName("li").length );
+        }
+
+//Task 3
+    //1.Получите div в переменную.
+    //2.Получите значение атрибута "data-widget-name" в переменную.
+    //3.Выведите его.
+
+    var widget = document.getElementById("widget");
+    var menu = widget.getAttribute("data-widget-name");
+    console.log(menu);
+
+//Task 4
+    //Сделайте желтыми внешние ссылки, добавив им класс external.
+    //Все ссылки без href, без протокола и начинающиеся с http://internal.com считаются внутренними.
+    //Все манипуляции производить с файлом links.html
+
+
+    var a = document.getElementsByTagName("a");
+    for(var  i = 0; i<a.length; i++){
+        if(
+            (a[i].getAttribute("href")!=null)
+            &&
+            ((a[i].innerHTML.indexOf("http")==0)
+            ||
+            (a[i].innerHTML.indexOf("ftp")==0))
+            &&
+            (a[i].innerHTML.indexOf("http://internal.com")!=0)
+
+        ){a[i].className = "external"}
+
+    }
+
+//Task 5
+    //Создайте кнопку в виде элемента <a> с заданным стилем, используя JavaScript.
+    //В примере ниже такая кнопка создана при помощи HTML/CSS.
+    // В вашем решении кнопка должна создаваться, настраиваться и добавляться в документ при помощи только JavaScript,
+    // без тегов <style> и <a>.
+
+var button = document.createElement("a");
+button.innerHTML = "Push me!";
+button.style.backgroundColor = "white";
+button.style.lineHeight = "2";
+button.style.fontSize = "2em";
+button.style.color = "red";
+button.style.textAlign = "center";
+button.style.width = "160px";
+button.style.height = "70px";
+button.style.borderRadius = "25px";
+button.style.border = "3px solid lightgreen";
+button.style.display = "block";
+document.body.appendChild(button);
